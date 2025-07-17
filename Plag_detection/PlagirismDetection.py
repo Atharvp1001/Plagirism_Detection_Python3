3387eb34eabcbe993f91cef68cabc0626d696668
Atharv Dhanaji Pol <118676468+Atharvp1001@users.noreply.github.com>
2025-07-17 17:44:13 +0530

    Just Some Value changes

--- Plag_detection/PlagirismDetection.py
+++ Plag_detection/PlagirismDetection.py
@@ -5,4 +5,4 @@ with open("demoFile1.txt") as fileOne, open("demoFile2.txt") as fileTwo:
     data_file2 = fileTwo.read()
 
     matches = SequenceMatcher(None, data_file1, data_file2).ratio()
-    print(f"The Plagieism content is {matches * 100}% ")
+    print(f"The Plagieism content is {matches * 100:.2f}% ")
