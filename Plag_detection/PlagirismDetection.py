d51b08f508feec574d25921eddf46d422f5191d1
Atharv Dhanaji Pol <118676468+Atharvp1001@users.noreply.github.com>
2025-07-17 17:21:52 +0530

    Basics Complete
    Added a simple working

---
+++ Plag_detection/PlagirismDetection.py
@@ -0,0 +1,8 @@
+from difflib import SequenceMatcher
+
+with open("demoFile1.txt") as fileOne, open("demoFile2.txt") as fileTwo:
+    data_file1 = fileOne.read()
+    data_file2 = fileTwo.read()
+
+    matches = SequenceMatcher(None, data_file1, data_file2).ratio()
+    print(f"The Plagieism content is {matches * 100}% ")
